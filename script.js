ee9a049de025a72145287c2e5bda7b9072d3ec42
BradleyGam <bradleygamble20@gmail.com>
2023-06-16 18:11:44 -0400

    feat: add color changing on hover

--- script.js
+++ script.js
@@ -1,12 +1,34 @@
 const sketchPad = document.querySelector("#sketch-container");
+createGrid(16);
 
-for (let i = 0; i < 16; i++) {
+
+function createGrid(size) {
+    sketchPad.innerHTML = "";
+    for (let i = 0; i < size; i++) {
+        const sketchRow = createRow();
+        sketchPad.appendChild(sketchRow);
+        for (let j = 0; j < size; j++) {
+            const sketchPixel = createPixel();
+            sketchRow.appendChild(sketchPixel);
+        }
+    }
+}
+
+function createRow() {
     const sketchRow = document.createElement("div");
     sketchRow.classList.add("row");
-    sketchPad.appendChild(sketchRow);
-    for (let i = 0; i < 16; i++) {
-        const sketchPixel = document.createElement("div");
-        sketchPixel.classList.add("pixel");
-        sketchRow.appendChild(sketchPixel);
-    }
+    return sketchRow;
+}
+
+function createPixel() {
+    const sketchPixel = document.createElement("div");
+    sketchPixel.classList.add("pixel");
+    sketchPixel.addEventListener("mouseover", function() {
+        sketchPixel.style.backgroundColor = `rgb(${createRandomColor()}, ${createRandomColor()}, ${createRandomColor()})`
+    });
+    return sketchPixel;
+}
+
+function createRandomColor() {
+    return Math.floor(Math.random() * 256);
 }
